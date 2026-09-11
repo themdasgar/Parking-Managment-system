40b7c5e164798beb9423b6fb4e6889143e56448c
Md Asgar <asgaransari251523@gmail.com>
2026-09-11 08:28:06 -0700

    update

--- config.py
+++ config.py
@@ -2,3 +2,6 @@ class Config:
     SECRET_KEY = 'mera-secret-key-Asgar@802219'
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:TUMHARA_PASSWORD@localhost/parking_system'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    <title>Signup</title>
+    <link rel="stylesheet" href="{{ url_for('static', filename='css/style.css') }}">
