a02536fa509def422317192b11cd1886a3b556a6
Md Asgar <asgaransari251523@gmail.com>
2026-09-11 08:29:44 -0700

    update

--- config.py
+++ config.py
@@ -3,5 +3,4 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:TUMHARA_PASSWORD@localhost/parking_system'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
-    <title>Signup</title>
-    <link rel="stylesheet" href="{{ url_for('static', filename='css/style.css') }}">
+    
